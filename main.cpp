40716fbba1d5f291324640db60342615ee45f6d7
JefferyHeo <tkrogj@naver.com>
2022-05-31 16:23:59 +0900

    ignore

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include "Scene.h"
 #include "Snake.h"
+#include "ScoreBoard.h"
+#include "Mission.h"
 #include <chrono>
 #include <thread>
 
@@ -11,69 +13,50 @@ int main()
 {
     // create default window including game screen, score board, user name
     Scene scene;
+    MapSet mapset;
     Snake snake;
+    ScoreBoard scoreBoard;
+    Mission missionBoard;
     int key;
 
     WINDOW *win;
     WINDOW *winGaming;
+    WINDOW *winScoreBoard;
+    WINDOW *winMission;
     scene.startScene();
-    
+
     keypad(stdscr, TRUE);
 
     // show Intro scene
-    win = scene.changeScene(0, snake.score);
-    
+    win = scene.changeScene(0, snake);
+
     // turn into Game scene
     getch();
-    
-    nodelay(stdscr, true);
-    timeout(500);
-    // halfdelay(10);
-    // nocbreak();
-    cbreak();
+    // cbreak();
+
     for (int i = 1; i < 5; i++)
     {
-        snake.setFirst();
-        win = scene.changeScene(i, snake.score);
-        while (true)
+        win = scene.changeScene(i, snake);
+        while (snake.getIsAlive())
         {
-            winGaming = scene.gamingScene(i, snake);
-            // nodelay(winGaming, true);
-            // getch();
-            key = KEY_RIGHT;
-            key = getch(); 
-            if(key == ERR){
-                if(snake.direction == 1) key = KEY_UP;
-                else if(snake.direction == 2) key = KEY_RIGHT;
-                else if(snake.direction == 3) key = KEY_DOWN;
-                else if(snake.direction == 4) key = KEY_LEFT;
-            }
+            winGaming = scene.gamingScene(i, mapset, snake);
+            winScoreBoard = scoreBoard.updateScoreBoard(snake);
+            winMission = missionBoard.updateMissionBoard(snake);
+            nodelay(stdscr, TRUE);
+            timeout(500);
+            cbreak();
             
+            key = KEY_RIGHT;
+            key = getch();
+            noecho();
+            snake.understandKey(key);
             if (key == 99)
-            {
                 break;
-            }
-            // this_thread::sleep_for(chrono::milliseconds(500));
-            switch (key)
-            {
-            case KEY_LEFT:
-                --snake.x;
-                snake.direction = 4;
-                break;
-            case KEY_RIGHT:
-                ++snake.x;
-                snake.direction = 2;
-                break;
-            case KEY_UP:
-                --snake.y;
-                snake.direction = 1;
-                break;
-            case KEY_DOWN:
-                ++snake.y;
-                snake.direction = 3;
-                break;
-            }
+
+            
+            
         }
+        snake.clearSnake();
     }
 
     // exit game
