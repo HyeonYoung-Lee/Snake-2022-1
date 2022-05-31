41b25f8da206f3c680e404dc81a5961c38871423
wynter <gusdud0122@kookmin.ac.kr>
2022-05-31 18:48:09 +0900

    Growth item 구현

---
+++ GrowthItem.cpp
@@ -0,0 +1,26 @@
+#include "GrowthItem.h"
+
+GrowthItem::GrowthItem()
+{
+    x = 0;
+    y = 0;
+}
+
+void GrowthItem::setX(int x)
+{
+    this->x = x;
+}
+void GrowthItem::setY(int y)
+{
+    this->y = y;
+}
+
+int GrowthItem::getX()
+{
+    return x;
+}
+
+int GrowthItem::getY()
+{
+    return x;
+}
---
+++ GrowthItem.h
@@ -0,0 +1,12 @@
+class GrowthItem
+{
+private:
+    int x, y;
+
+public:
+    GrowthItem();
+    void setX(int x);
+    void setY(int y);
+    int getX();
+    int getY();
+};
