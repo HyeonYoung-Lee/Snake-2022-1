fe9e3a15703f8cb635057d0f24ba7639b833c52a
wynter <gusdud0122@kookmin.ac.kr>
2022-06-01 05:27:46 +0900

    Item 추가

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <chrono>
 #include <thread>
 
+#include "Item.h"
 using namespace std;
 
 int main()
@@ -18,6 +19,8 @@ int main()
     ScoreBoard scoreBoard;
     Mission missionBoard;
     int key;
+
+    int itemTime = 0;
 
     WINDOW *win;
     WINDOW *winGaming;
@@ -33,28 +36,33 @@ int main()
     // turn into Game scene
     getch();
     // cbreak();
-
     for (int i = 1; i < 5; i++)
     {
+        Item growthItem(5); ///////////// growth와 poison 의 시드가 같아서 위치가 같게 나오는 것 같음 수정 필요
+        Item poisonItem(6);
         win = scene.changeScene(i, snake);
         while (snake.getIsAlive())
         {
-            winGaming = scene.gamingScene(i, mapset, snake);
+            if ((itemTime != 0) && (itemTime % 10 == 0)) // 5초마다 resetItem
+            {
+                growthItem.resetItem(); ///////////// growth와 poison 의 시드가 같아서 위치가 같게 나오는 것 같음 수정 필요
+                poisonItem.resetItem();
+            }
+
+            winGaming = scene.gamingScene(i, mapset, snake, growthItem, poisonItem);
             winScoreBoard = scoreBoard.updateScoreBoard(snake);
             winMission = missionBoard.updateMissionBoard(snake);
             nodelay(stdscr, TRUE);
             timeout(500);
             cbreak();
-            
+
             key = KEY_RIGHT;
             key = getch();
             noecho();
             snake.understandKey(key);
             if (key == 99)
                 break;
-
-            
-            
+            itemTime += 1;
         }
         snake.clearSnake();
     }
