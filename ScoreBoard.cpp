6289083277e4dfec38d2c0d433d6fab2a714e7c1
김진재 <jinjae.dev@gmail.com>
2022-06-03 00:23:24 +0900

    SnakeDeadSpecified

--- ScoreBoard.cpp
+++ ScoreBoard.cpp
@@ -1,14 +1,10 @@
-#include <ncurses.h>
-#include <iostream>
-#include <string>
 #include "ScoreBoard.h"
-#include "Snake.h"
 using namespace std;
 
 ScoreBoard::ScoreBoard()
 {
-    width = 15;
-    height = 5;
+    width = 19;
+    height = 7;
 }
 
 WINDOW *ScoreBoard::updateScoreBoard(Snake snake)
@@ -18,9 +14,25 @@ WINDOW *ScoreBoard::updateScoreBoard(Snake snake)
     wborder(winScore, '|', '|', '-', '-', '+', '+', '+', '+');
 
     mvwprintw(winScore, 1, 1, "Score Board");
-    std::string S = std::to_string(snake.getScore());
-    auto charScore = S.c_str();
-    mvwprintw(winScore, 2, 1, charScore);
+	// B: Render
+	std::string Length = std::string("B: ") + std::to_string(snake.getCurrentLength());
+	Length += std::string(" / ");
+	Length += std::to_string(snake.getMaxLength());
+    auto charLength = Length.c_str();
+    mvwprintw(winScore, 2, 1, charLength);
+	// +: Render
+	std::string Growth = std::string("+: ") + std::to_string(snake.getGrowthItems());
+    auto charGrowth = Growth.c_str();
+    mvwprintw(winScore, 3, 1, charGrowth);
+	// -: Render
+	std::string Poison = std::string("-: ") + std::to_string(snake.getPoisonItems());
+    auto charPoison = Poison.c_str();
+    mvwprintw(winScore, 4, 1, charPoison);
+    wrefresh(winScore);
+	// G: Render
+	std::string Gate = std::string("G: ") + std::to_string(snake.getGateUses());
+    auto charGate = Gate.c_str();
+    mvwprintw(winScore, 5, 1, charGate);
     wrefresh(winScore);
 
     return winScore;
--- ScoreBoard.h
+++ ScoreBoard.h
@@ -1,4 +1,8 @@
+#include <ncurses.h>
+#include <iostream>
+#include <string>
 #include "Snake.h"
+
 class ScoreBoard
 {
 protected:
